f3dc9fb3116b3ec7c49ad94a96fe92316147a503
Meghan Hein <meghan.hein@gmail.com>
2021-07-06 17:27:34 -0700

    feat: update subschema options to include executor

--- index.js
+++ index.js
@@ -1,67 +1,43 @@
 'use strict'
 
 const fp = require('fastify-plugin')
-const { createPostGraphileSchema, withPostGraphileContext } = require('postgraphile')
+const { print } = require('graphql')
+const { createPostGraphileSchema } = require('postgraphile')
 const { stitchSchemas } = require('@graphql-tools/stitch')
-const { delegateToSchema } = require('@graphql-tools/delegate')
-const DEFAULT_INFLECTOR = require('@graphile-contrib/pg-simplify-inflector')
+const { makeQueryRunner } = require('./makeQueryRunner.js')
 
 async function mercuriusPostgraphile (fastify, opts) {
   const { graphql } = fastify
   const {
     connectionString,
-    pgClient,
+    graphileSchemaOpts = {},
     instanceName = 'public',
+    mergeOpts = {},
+    pgClient,
     transformsOpts,
-    mergeOpts
   } = opts
-
-
-  const defaultSettings = {
-    subscriptions: true,
-    watchPg: true,
-    dynamicJson: true,
-    setofFunctionsContainNulls: false,
-    ignoreRBAC: false,
-    ignoreIndexes: false,
-    appendPlugins: [DEFAULT_INFLECTOR],
-    enableQueryBatching: true,
-    legacyRelations: 'omit'
-  }
 
   const postgraphileSchema = await createPostGraphileSchema(
     connectionString,
     instanceName,
-    defaultSettings
-  )
-
-  function createProxyingResolverWithPGClient ({
-    subschemaConfig,
-    operation,
-    transforms,
-    transformedSchema
-  }) {
-    return (_parent, _args, context, info) => withPostGraphileContext(
     {
-      pgPool: pgClient,
-      // jwtToken: jwtToken,
-      // jwtSecret: "...",
-      // pgDefaultRole: "..."
-    }, (pgContext) =>
-      delegateToSchema({
-      schema: subschemaConfig,
-      operation,
-      context: {...context, ...pgContext},
-      info,
-      transformedSchema
-    }))
-  }
-
+      subscriptions: true,
+      dynamicJson: true,
+      setofFunctionsContainNulls: false,
+      ignoreRBAC: false,
+      ignoreIndexes: false,
+      legacyRelations: 'omit',
+      ...graphileSchemaOpts
+    }
+  )
   graphql.replaceSchema(stitchSchemas({
     subschemas: [
       {
         schema: postgraphileSchema,
-        createProxyingResolver: createProxyingResolverWithPGClient,
+        executor: async ({ document, variables }) => {
+          const runner = await makeQueryRunner(postgraphileSchema, pgClient)
+          return runner(print(document), variables)
+        },
         transforms: transformsOpts,
         merge: mergeOpts
       },
